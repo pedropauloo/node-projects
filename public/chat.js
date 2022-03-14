456c3b5a184fe0d1c67649a40c9aa05a5abe4fe2
Pedro Paulo Lucas de Lira <pedrolira@NATALCENTER.LOCAL>
2022-03-14 07:34:30 -0300

    socket receiving messages and sending

--- public/chat.js
+++ public/chat.js
@@ -1,46 +1,46 @@
 const socket = io();
 
-var roomSelect = document.querySelector('#room-id');
-var usernameInput = document.querySelector('#username-id');
-var username = 'anonymous';
-var room = 'room-1';
+var roomSelect = document.querySelector("#room-id");
+var usernameInput = document.querySelector("#username-id");
+var username = "anonymous";
+var room = "room-1";
 
 // emit -> emitir informação
 // on -> escutando informação
 
-function joinRoom(){
-    socket.emit("join-room", {username, room}, (response) => {
-        response.map((data) => {
-            createMessage(data);
-        })
+function joinRoom() {
+  socket.emit("join-room", { username, room }, (response) => {
+    response.map((data) => {
+      createMessage(data);
     });
+  });
 }
 
-roomSelect.addEventListener('change', (e) => {
-    room = e.target.value;
-    joinRoom()
-})
+roomSelect.addEventListener("change", (e) => {
+  room = e.target.value;
+  joinRoom();
+});
 
-usernameInput.addEventListener('change', (e) => {
-    username = e.target.value ?? 'anonymous';
-    joinRoom()
-})
+usernameInput.addEventListener("change", (e) => {
+  username = e.target.value ?? "anonymous";
+  joinRoom();
+});
 
-document.querySelector('#message-id').addEventListener('keypress', (e) => {
-    if(e.key === 'Enter' && e.target.value !== ''){
-     const message = e.target.value;   
-     const data = {room, message, username};
-     e.target.value = '';
-     socket.emit("send-message", data);
-    }
-})
+document.querySelector("#message-id").addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && e.target.value !== "") {
+    const message = e.target.value;
+    const data = { room, message, username };
+    e.target.value = "";
+    socket.emit("send-message", data);
+  }
+});
 
-socket.on('message', (data) => {
-    createMessage(data)
-})
-function createMessage(data){
-    const message = document.createElement('p');
-    const messageContainer = document.querySelector('#chat');
-    message.innerHTML = `<b>${data.username}:</b> ${data.text}`;
-    messageContainer.append(message);
+socket.on("message", (data) => {
+  createMessage(data);
+});
+function createMessage(data) {
+  const message = document.createElement("p");
+  const messageContainer = document.querySelector("#chat");
+  message.innerHTML = `<b>${data.username}:</b> ${data.text}`;
+  messageContainer.append(message);
 }
--- src/websocket.ts
+++ src/websocket.ts
@@ -1,12 +1,14 @@
 import { io } from "./http";
 
 interface UserRoom {
+  id: string;
   socket_id: string;
   username: string;
   room: string;
 }
 
 interface Message {
+  id: string;
   room: string;
   username: string;
   text: string;
@@ -17,17 +19,18 @@ const users: UserRoom[] = [];
 const messages: Message[] = [];
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (data, callback) => {
+  socket.on("chat.join", (data, callback) => {
     socket.join(data.room);
 
     const userInRoom = users.find(
-      (user) => user.username === data.username && user.room === data.room
+      (user) => user.id === data.id && user.room === data.room
     );
 
     if (userInRoom) {
       userInRoom.socket_id = socket.id;
     } else {
       users.push({
+        id: data.id,
         socket_id: socket.id,
         username: data.username,
         room: data.room,
@@ -38,19 +41,18 @@ io.on("connection", (socket) => {
     callback(messagesRoom);
   });
 
-  socket.on("send-message", (data) => {
+  socket.on("chat.message", (data) => {
     // Salva as mensagens
     const message: Message = {
       room: data.room,
+      id: data.id,
       username: data.username,
       text: data.message,
       createdAt: new Date(),
     };
-
     messages.push(message);
-
     // Envia para tela de usuários com base na sala
-    io.to(data.room).emit("message", message);
+    io.emit("chat.message", message);
   });
 });
 
